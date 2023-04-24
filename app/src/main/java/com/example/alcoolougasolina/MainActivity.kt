3a9c6af270fcd6e0534576eea2a6aea8944825aa
Victor misael <misael1805@gmail.com>
2023-04-23 21:23:14 -0300

    versão inicial funcional

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -5,33 +5,52 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var percentual:Double = 0.7
     var buttonState:Boolean = false;
+    val ALCOOL="Alcool vale a pena"
+    val GASOLINA="Gasolina vale a pena"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("PDM23","No onCreate, $percentual")
 
         val btCalc: Button = findViewById(R.id.btCalcular)
-        val switch: Switch = findViewById(R.id.swPercentual);
+        val switch: Switch = findViewById(R.id.swPercentual)
+        val alcoolTxt:EditText = findViewById(R.id.precoAlcool)
+        val gasolinaTxt:EditText = findViewById(R.id.precoGasolina)
+        val resultado:TextView = findViewById(R.id.resultado)
         switch.setOnCheckedChangeListener{ _,isChecked ->
             buttonState=isChecked
             Log.d("PDM_teste","trocou estado para, $buttonState");
         }
 
 
-        btCalc.setOnClickListener(View.OnClickListener {
-            //código do evento
-            percentual=0.75
-            Log.d("PDM23","No btCalcular, $percentual")
-        })
+        btCalc.setOnClickListener {
+            //código do., evento
+            val percentual: Double = if (buttonState) 0.75 else 0.70
+
+
+            try {
+                // some code
+                val alcool: Double = alcoolTxt.text.toString().toDouble()
+                val gasolina: Double = gasolinaTxt.text.toString().toDouble()
+                val divisao = alcool / gasolina
+                Log.d("PDM23","percentual testado $percentual")
+                Log.d("PDM23","Divisão entre alcool e gasolina $divisao")
+                resultado.text = if (percentual > divisao) ALCOOL else GASOLINA
+            } catch (e: java.lang.NumberFormatException) {
+                // handler
+                Log.e("PDM23", "Formato invalido");
+            }
+            Log.d("PDM23", "No btCalcular, $percentual")
+        }
     }
 override fun onResume(){
     super.onResume()
-    Log.d("PDM23","No onResume, $percentual")
+    Log.d("PDM23","No onResume,")
 }
 override fun onStart(){
     super.onStart()

48f016854e6e55b452f75320f08fc546fc831435
Victor misael <misael1805@gmail.com>
2023-05-15 19:39:37 -0300

    Atualiza acessibilidade alcool ou gasolina

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -79,7 +79,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
-
+        super.onConfigurationChanged(newConfig)
     }
 
     override fun onResume() {
